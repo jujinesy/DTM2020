5edabfc04cde9ed2ed51b594b88ca1517c5ed29a
unknown <jujinesy@gmail.com>
2020-05-03 19:32:51 +0900

    connect unity cloud

---
+++ Assets/Script/Intro Script/PanelManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelManager : MonoBehaviour
+
+{
+    public GameObject Panel1;
+    public GameObject Panel3;
+
+
+    public void OpenInfoPanel()
+    {
+        if (Panel1 != null)
+        {
+            bool isActive = Panel1.activeSelf;
+            Panel1.SetActive(!isActive);
+            Panel3.SetActive(false);
+
+
+        }
+    }
+
+    public void OpenLogInPanel()
+    {
+        if(Panel3 != null)
+        {
+            bool isActive = Panel3.activeSelf;
+            Panel3.SetActive(!isActive);
+            Panel1.SetActive(false);
+
+        }
+    }
+}
